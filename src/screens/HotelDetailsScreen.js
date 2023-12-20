e89b62a067d3a45410a056679d9dc3b2bafd67ad
Mehmetali Demirtas <73759052+salesin58@users.noreply.github.com>
2023-12-20 19:28:13 +0300

    Hotel Details Screen style test

--- src/screens/HotelDetailsScreen.js
+++ src/screens/HotelDetailsScreen.js
@@ -1,25 +1,68 @@
 import React from "react";
-import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Dimensions,
+  FlatList,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import StarRating from "../components/Card/StarRating";
+const { width } = Dimensions.get("window");
 
 export default function HotelDetailsScreen({ navigation, route }) {
   const { hotelName, city, hotelStar, photoURLs, capacity } = route.params;
-
+  const renderImageItem = ({ item }) => (
+    <Image source={{ uri: item }} style={styles.imageItem} />
+  );
   return (
-    <ScrollView style={styles.container}>
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>HotelDetailsScreen Screen</Text>
-        <View>
-          <Text>{hotelName}</Text>
-          <Text>{city}</Text>
-          <Text>{hotelStar}</Text>
+    <View style={styles.container}>
+      {photoURLs.length > 0 && (
+        <View
+          style={{
+            margin: 15,
+            marginBottom: 10,
+            justifyContent: "center",
+          }}
+        >
+          <FlatList
+            data={photoURLs}
+            renderItem={renderImageItem}
+            keyExtractor={(item, index) => index.toString()}
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+          />
         </View>
+      )}
+      <Text style={{ fontWeight: "bold", fontSize: 18, marginTop: 5 }}>
+        {hotelName}
+      </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text style={styles.city}>Konum: {city}</Text>
+        <StarRating rating={hotelStar} />
       </View>
-    </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: "#fff",
+  },
+  imageItem: {
+    width,
+    height: 250,
+    resizeMode: "cover",
   },
 });
