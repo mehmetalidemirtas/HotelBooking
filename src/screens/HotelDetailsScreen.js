eb40a6ddbff618c7b5f599b154a3129621be60c5
Mehmetali Demirtas <mehmetalidemirts@gmail.com>
2023-12-20 19:58:30 +0300

    Pick Date Component completed

--- src/screens/HotelDetailsScreen.js
+++ src/screens/HotelDetailsScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Dimensions,
   FlatList,
@@ -7,20 +7,45 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
   View,
 } from "react-native";
 import StarRating from "../components/Card/StarRating";
 import Button from "../components/Button/Button";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import Input from "../components/Input/Input";
+
 const { width } = Dimensions.get("window");
 
 export default function HotelDetailsScreen({ navigation, route }) {
   const { hotelName, city, hotelStar, photoURLs, capacity } = route.params;
+
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode("date");
+  };
+
   const renderImageItem = ({ item }) => (
     <Image source={{ uri: item }} style={styles.imageItem} />
   );
 
   const bookingHotel = () => {
-    console.log("book hotel...");
+    console.log("book hotel in here...");
   };
 
   return (
@@ -55,6 +80,24 @@ export default function HotelDetailsScreen({ navigation, route }) {
           <Text style={styles.city}>Konum: {city}</Text>
           <StarRating rating={hotelStar} />
         </View>
+        <Button
+          onPress={showDatepicker}
+          title={"Otele Giriş Tarihi: " + date.toLocaleDateString()}
+        />
+        <Button
+          onPress={showDatepicker}
+          title={"Otelden Çıkış Tarihi: " + date.toLocaleDateString()}
+        />
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour={true}
+            onChange={onChange}
+            minimumDate={new Date()}
+          />
+        )}
       </View>
       <Button title="Rezervasyon Yap" onPress={bookingHotel} />
     </View>
